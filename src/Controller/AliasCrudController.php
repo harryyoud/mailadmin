657dc82c23e72dbbb55fddd3b989d2f22ea8458b
Harry Youd <harry@harryyoud.co.uk>
2021-02-03 17:42:48 +0000

    Alias: order by source email

--- src/Controller/AliasCrudController.php
+++ src/Controller/AliasCrudController.php
@@ -4,13 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Alias;
 use App\Filter\ConcatFilter;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
-use EasyCorp\Bundle\EasyAdminBundle\Filter\ComparisonFilter;
-use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class AliasCrudController extends AbstractCrudController {
     public static function getEntityFqcn(): string {
@@ -51,6 +54,13 @@ class AliasCrudController extends AbstractCrudController {
         return $filters
             ->add(ConcatFilter::new(['source_username', '@', 'source_domain:domain'], 'From email'))
             ->add(ConcatFilter::new(['destination_username', '@', 'destination_domain'], 'To email'))
+        ;
+    }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder {
+        return parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters)
+            ->addOrderBy('entity.source_domain', 'ASC')
+            ->addOrderBy('entity.source_username', 'ASC')
         ;
     }
 }
