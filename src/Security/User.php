8818ef1ac71fab2d20996658c313592d4c58b589
Harry Youd <harry@youd.family>
2024-05-17 15:26:23 +0100

    Try to use IDP roles

---
+++ src/Security/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements UserInterface {
+    private string $username;
+    private array $roles;
+
+    public function __construct(string $username, array $roles) {
+        $this->username = $username;
+        $this->roles = [
+            'ROLE_USER',
+            'ROLE_OAUTH_USER',
+            ...$roles,
+        ];
+    }
+
+    public function getUserIdentifier(): string {
+        return $this->username;
+    }
+
+    /**
+     * @return array<int, string>
+     */
+    public function getRoles(): array {
+        return $this->roles;
+    }
+
+    public function getPassword(): ?string {
+        return null;
+    }
+
+    public function getSalt(): ?string {
+        return null;
+    }
+
+    public function getUsername(): string {
+        return $this->getUserIdentifier();
+    }
+
+    public function eraseCredentials(): void {
+    }
+
+    public function equals(UserInterface $user): bool {
+        return $user->getUserIdentifier() === $this->username;
+    }
+}
